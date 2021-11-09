607d5096b03339ef0a1d9706c65d79cad69bf61f
Hector <gar-mira@hotmail.com>
2021-11-09 11:08:29 -0600

    comments to the hastable file

--- hashTable.h
+++ hashTable.h
@@ -1,3 +1,8 @@
+
+/*
+Class that creates the rows of the hashtable, with the capability
+of inputing key on the left and value on the right.
+*/
 template<typename K, typename T> /* K key, T value*/
 class HashPair{
   public:
@@ -10,7 +15,9 @@ class HashPair{
      HashPair(){}
 
 };
-
+/*
+Class for creating a hashtable.
+*/
 template<typename K, typename T> /* K key, T value*/
 class Hashtable{
     private:
@@ -25,81 +32,112 @@ class Hashtable{
         }
 
     public:
+    /*
+    Builder
+    */
       Hashtable(int size = 20){
         this->size = size;
         tabla = new HashPair<K,T>[size];  // hashpair array creation
         for(int i = 0; i < size; i++) tabla[i] = HashPair<K,T>();
         null_hash = tabla[0];
       }
+      /*
+      Destructor
+      */
       ~Hashtable(){
         delete tabla;
         size = 0;
         empty = true;
       }
-
+      /*
+      Method for inputting rows to the hastable.
+      */
       bool put(K key, T value){
          int posicion = fh(key);
          this ->empty = false; 
          tabla[posicion] = HashPair<K,T>(key, value);
          return 1;
       }
-
+    /*
+    Method for getting a value depending of some Key as parameter.
+    */
       T get(K key){
         return tabla[fh(key)].value;
       }
-
+    /*
+    Method for printing the whole hashtable-
+    */
       void print(){
         for(int i = 0; i < size; i++){
           cout <<"i: " << i << " k: " << tabla[i].key  << " v: " << tabla[i].value;
           cout << endl;
         }
       }
+    /*
+    Method for deleting all values from the hashtable.
+    */
       void clear(){
         for(int i = 0; i < size; i++){
           tabla[i] = null_hash;
         }
-
       }
+    /*
+    Method for checking if there is some key.
+    */
       bool contains_key(K k){
         if(tabla[fh(k)].key == k) return true;
         return false;
       }
-
+      /*
+      Method for getting the value depending of key, if it doestn exist
+      on hastable, it will return a default value.
+      */
       T get_or_default(K k, T def = 0){
         if(contains_key(k)) return tabla[fh(k)].value;
         return def;
       }
-
-
+      /*
+      Method for checking whether the hashtable is full or empty.
+      */
       bool is_empty(){
         for(int i = 0; i < size; i++) if(tabla[i].value != null_hash.value) return false;
         return true; 
       }
-
+      /*
+      Method that takes another hastable of the same type and inputs all the value.
+      */
       void put_all(const Hashtable<K,T>& other){
         for(int i = 0; i < size; i++){
           if(other.tabla[i].key != tabla[i].key) tabla[i].key = other.tabla[i].key;
           tabla[i].value = other.tabla[i].value;
         }
       }
-
+      /*
+      Method for removing some row  depending of its key 
+      */
       bool remove(K key){
         if(contains_key(key))tabla[fh(key)] = null_hash;
         else return false;
         return true;
       }
-
+      /*
+      Method that return the number of not empty rows
+      */
       int the_size(){
         int counter = 0;
         for(int i = 0; i < size; i++)if(tabla[i].value != null_hash.value) counter++;
         return counter;
       }
-
+      /*
+      operator overloading for checking whether two hastables are the same or not.
+      */
       bool operator == (const Hashtable<K, T> & other) const{
         if(size != other.size) return false;
         for(int i = 0; i < size; i++) if(tabla[i].value != other.tabla[i].value && tabla[i].key != other.tabla[i].key) return false;
         return true;
       }
+
+
 
       // additional methods
       int no_elements_in(){
